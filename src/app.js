552bd9ec7d6b17b711b4e462b561c136113d9cbf
princ <princeptl123456@gmail.com>
2022-11-07 23:34:39 +0530

    day 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const hbs = require('hbs');
+const db = require('./utils/dbconnection');
+const userModel = require("./models/user");
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,14 +22,50 @@ hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
-
+app.use(session({secret: 'secret', resave: true, saveUninitialized: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
-    res.render('index');
+app.get("/",  (req, res) => {
+    res.render('index',{username : req.session.username});
 });
+
+app.get("/register",(req,res)=>{
+    res.render('register');
+});
+
+app.post("/create_user",(req,res)=>{
+    const user = new userModel(req.body);
+
+    user.save().then((result) => {
+        res.redirect('/');
+    }, (error) => {
+        res.status(501).send(error)
+    })
+});
+
+app.get("/login",(req,res)=>{
+    res.render('login');
+});
+
+app.post("/auth_user",(req,res)=>{
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    userModel.findOne({username : username , password : password }).then((result) => {
+        if(!result) res.status(201).send("Invalid Passsword/Username");
+        else{
+            req.session.loggedin = true;
+            req.session.username = username;
+            res.redirect('/');
+        }
+    }, (error) => {
+        res.status(501).send(error)
+    })
+});
+
 
 app.get('*', (req, res) => {
     res.render('404');
---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},{collection:'user',
+    versionKey: false
+});
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
